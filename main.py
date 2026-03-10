34fcbb4a9d5b83399c04f6105185985fa908ea0d
Raktabh <gitadb70@gmail.com>
2026-03-10 21:29:50 +0530

    add POST /users endpoint with pydantic validation

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+from pydantic import BaseModel, field_validator
 from fastapi import FastAPI
 
 
-app=FastAPI()
+class UserCreate(BaseModel):
+    email: str
 
-@app.get('/health')
+    @field_validator('email')
+    @classmethod
+    def validate_email(cls,value):
+        if '@' not in value:
+            raise ValueError('Invalid Email')
+        return value
+
+app = FastAPI()
+
+
+@app.get("/health")
 def health_check():
-    return{
-        "status":"ok"
-    }
+    return {"status": "ok"}
+
+
+# post request
+
+
+@app.post("/users")
+def create_user(user: UserCreate):
+    return {"email": user.email}
