d14bfbd87a6a185a6a37bf88d06ec6103d89627e
Raktabh <gitadb70@gmail.com>
2026-03-11 09:24:32 +0530

    add GET /notes with query parameters and validation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, field_validator
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
 
 
 
@@ -33,3 +33,14 @@ def health_check():
 @app.post("/users", status_code=201, response_model=UserResponse)
 def create_user(user: UserCreate):
     return {"email": user.email}
+
+@app.get("/users{user_id}")
+def get_user(user_id : int ):
+    return{
+        "user_id":user_id
+    }
+@app.get("/notes")
+def get_notes(page:int=Query(default=1,ge=1),limit:int=Query(default=1,ge=1, le=100)):
+    return{
+        "page":page, "limit":limit
+    }
