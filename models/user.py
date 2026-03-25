234e7d8d1f921ab5d2d346a4cf234b2c0c234ef7
Raktabh <gitadb70@gmail.com>
2026-03-25 23:10:51 +0530

    added alembic and will be building auth services

--- models/user.py
+++ models/user.py
@@ -1,11 +1,7 @@
 from datetime import datetime, timezone
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship
-from models.base import Base  # if you want to put Base in a separate file (optional)
 from .base import Base
-
-
-
 
 
 class User(Base):
@@ -13,11 +9,11 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, nullable=False, unique=True, index=True)
+    hashed_password = Column(String, nullable=False)
     created_at = Column(
         DateTime(timezone=True),
         default=lambda: datetime.now(timezone.utc),
         nullable=False,
     )
 
-    # Relationship to notes
     notes = relationship("Note", back_populates="owner")
